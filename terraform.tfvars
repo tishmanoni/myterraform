f97ab3e855e0f035eb19ade9186910221fb5102a
tishmanoni <mide999@gmail.com>
2025-02-27 20:08:17 +0000

    FIXED

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
 aws_region = "us-east-1"
-aws_access_key = ""
-aws_secret_key = ""
+# aws_access_key = ""
+# aws_secret_key = ""
 subnet_prefix = "10.0.10.0/24"
--- variables.tf
+++ variables.tf
@@ -3,15 +3,15 @@ variable "aws_region" {
   type = string
 }
 
-variable "aws_access_key" {
-  description = "access key"
-  type = string
-}
+# variable "aws_access_key" {
+#   description = "access key"
+#   type = string
+# }
 
-variable "aws_secret_key" {
-  description = "secret key"
-  type = string
-}
+# variable "aws_secret_key" {
+#   description = "secret key"
+#   type = string
+# }
 
 variable "subnet_prefix" {
   description = "my subnet prefix"
